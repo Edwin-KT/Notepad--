666b2e44d94bd5b1f0fc455627db169cd9d8d333
Edwin Kantor-Tomcek <77319863+Edwin-KT@users.noreply.github.com>
2026-03-06 20:19:41 +0200

    fix inheritance

--- Notepad--/Notepad--/ViewModels/FileSystemViewModel.cs
+++ Notepad--/Notepad--/ViewModels/FileSystemViewModel.cs
@@ -1,14 +1,12 @@
 ﻿using Notepad__.Models;
+using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.CompilerServices;
 
 namespace Notepad__.ViewModels
 {
-    public class FileSystemViewModel : INotifyPropertyChanged
+    public class FileSystemViewModel : BaseViewModel
     {
-        // Radacinile arborelui = partitiile de pe calculator (C:\, D:\, etc.)
         public ObservableCollection<FileSystemItem> Drives { get; }
 
         private FileSystemItem _selectedItem;
@@ -19,7 +17,6 @@ namespace Notepad__.ViewModels
         }
 
         // Event prin care anuntam MainViewModel sa deschida un fisier
-        // Il vom folosi la subpunctul 5 (dublu click)
         public event Action<string> OpenFileRequested;
 
         public FileSystemViewModel()
@@ -31,16 +28,8 @@ namespace Notepad__.ViewModels
 
         public void OnItemDoubleClicked(FileSystemItem item)
         {
-            // Ignoram dublu click pe directoare
             if (item == null || item.IsDirectory) return;
-
-            // Verificam daca fisierul ar putea fi text
-            // (incercam sa il deschidem si prindem eroarea daca e binar)
             OpenFileRequested?.Invoke(item.FullPath);
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
